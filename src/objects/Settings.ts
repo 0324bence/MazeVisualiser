886b5c6f583fa33c83e3b3cf2d3a86fbad3f74fa
0324bence <bence20050324@gmail.com>
2023-04-25 07:57:00 +0200

    Add custom, changeable variables to settings

--- src/objects/Settings.ts
+++ src/objects/Settings.ts
@@ -1,8 +1,17 @@
+import { Coords } from "./AStarPathFinding";
+
 class Settings {
     public static SIZE = 800;
     public static CELL_COUNT = 20;
     public static CELL_SIZE = Settings.SIZE / Settings.CELL_COUNT;
     public static FPS = 60;
+
+    public static CUSTOMS = {
+        startPos: [5, 5] as Coords,
+        endPos: [Settings.CELL_COUNT - 4, Settings.CELL_COUNT - 4] as Coords,
+        useDiagonals: true,
+        showCoords: false
+    };
 }
 
 export default Settings;
